02e6763470f28f95593fa30fce5cf25b5257fa4b
madelinedulchin <madeline.dulchin@gmail.com>
2022-09-19 21:44:56 -0400

    commit tree 72c7ce4a05c50c62fe54eee4001a158d7b7fe854

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function  distanceFromHqInBlocks(street) {
+    const headquarters = 42
+    if (street > 42) {
+        return street-headquarters
+    }
+    else  {
+        return headquarters-street
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) *264
+}
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start){
+    return (destination - start) *264 }
+    else {
+        return (start -destination) *264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) -400) * .02
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2500 ) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
